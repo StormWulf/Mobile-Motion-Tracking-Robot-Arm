50d166f51a81b3ac38d07b23bf6fb039422ac7bb
Jeff R <jruocco36@gmail.com>
2016-03-29 19:38:52 -0400

    Comments

--- Robot arm/Arduino Code/ArmControl/ArmControl.ino
+++ Robot arm/Arduino Code/ArmControl/ArmControl.ino
@@ -1,7 +1,21 @@
 //Setup Output
 
+/*
+Servo# - Function
+0 - X movement
+1 - Z movement
+2 - Y movement
+3 - Wrist
+4 - Gripper
+31 - Front left wheel
+30 - Back left wheel
+16 - Front right wheel
+17 - Back right wheel
+*/
+
+
 #include <SoftwareSerial.h>
-SoftwareSerial Xbee(2,3);
+SoftwareSerial Xbee(2,3);   // Software serial for Xbee communication
 
 int ledPin_3 = 13;
 
@@ -12,8 +26,7 @@ byte inputByte_2;
 byte inputByte_3;
 byte inputByte_4;
 
-boolean connected = false;
-boolean cont = true;
+// Servo max/min positions
 int min_y = 1080;
 int max_y = 1970;
 int min_x = 1200;
@@ -21,35 +34,43 @@ int max_x = 2500;
 int min_z = 860;
 int max_z = 1800;
 
+boolean connected = false;
+
 //Setup
 void setup() {
-  pinMode(ledPin_3, OUTPUT);
-  Serial.begin(9600);
-  Xbee.begin(9600);
-  digitalWrite(ledPin_3, HIGH);//
-  delay(250);//
-  digitalWrite(ledPin_3, LOW);//
-  delay(250);//
-  digitalWrite(ledPin_3, HIGH);//
-  delay(250);//
-  digitalWrite(ledPin_3, LOW);//
-  delay(250);//
-  Xbee.println("#0 P1890 #1 P1410 #2 P1630 #3 P1300 #4 P1500 S50");
+    pinMode(ledPin_3, OUTPUT);
+    Serial.begin(9600);
+    Xbee.begin(9600);
+    digitalWrite(ledPin_3, HIGH);//
+    delay(250);//
+    digitalWrite(ledPin_3, LOW);//
+    delay(250);//
+    digitalWrite(ledPin_3, HIGH);//
+    delay(250);//
+    digitalWrite(ledPin_3, LOW);//
+    delay(250);//
+    
+    // Set arm to initial position
+    reset();
 }
 
 //Main Loop
 void loop() {
+    // Connect to Kinect
     if (!connected){
         connect();
     }
+    // When connected, wait for serial input
     else{
         if (Serial.available() > 0){
             String serialIn = Serial.readStringUntil('\n');
             int posJoint = 0;
-            if (serialIn == "reset"){
-              reset(); 
+            if (serialIn == "reset"){   // Reset signal
+              reset();                  // Reset arm to initial position     
               return;
             }
+            
+            // Parse positions from serial input
             int posX = serialIn.indexOf(',', posJoint + 1);
             int posY = serialIn.indexOf(',', posX + 1);
             int posZ = serialIn.indexOf(',', posY + 1);
@@ -58,56 +79,58 @@ void loop() {
             String y = serialIn.substring(posY+1, posZ);
             String z = serialIn.substring(posZ+1);
             String data = joint + "," + x + "," + y + "," + z;
-            if (joint == "HandRight"){
+            
+            if (joint == "HandRight"){  // Right hand instruction
                 Serial.println("Arduino received command for right hand.");
-                    moveY(y.toFloat());
-                    moveX(x.toFloat());
-                    moveZ(z.toFloat());
-                    Xbee.println("#3 P1300");       // Move wrist to safe position
-            }
-            else{
-                //Serial.println("New data received: " + serialIn);
+                moveY(y.toFloat());     // Move Y servo to position
+                moveX(x.toFloat());     // Move X servo to position
+                moveZ(z.toFloat());     // Move Z servo to position
+                Xbee.println("#3 P1300");   // Move wrist to safe position
             }
         }
     }
 }
 
+// Move Y servo to specified position
 void moveY(float y){
-    Xbee.println(27, 'i');      // Cancel any previous commands
+    Xbee.println(27, 'i');                  // Cancel any previous commands
 //    float moveTo = ((-1483.333*y)+1525);  // Original equation
-    float moveTo = (-1112*y)+1525;  // Slower movement
-    if (moveTo > max_y) moveTo = max_y;
-    else if (moveTo < min_y) moveTo = min_y;
+    float moveTo = (-1112*y)+1525;          // Slower movement
+    if (moveTo > max_y) moveTo = max_y;     // Clamp values higher than max
+    else if (moveTo < min_y) moveTo = min_y;    // Clamp values lower than min
     Xbee.print("#2 P");
     Xbee.print(moveTo);
     Xbee.print(" S600 T10");
     Xbee.println("");
 }
 
+// Move X servo to specified position
 void moveX(float x){
-    Xbee.println(27, 'i');      // Cancel any previous commands
-//    float moveTo = (2167*x)+1417; // Original equation
-    float moveTo = (1625*x)+1525;   // Slower movement
-    if (moveTo > max_x) moveTo = max_x;
-    else if (moveTo < min_x) moveTo = min_x;
+    Xbee.println(27, 'i');                  // Cancel any previous commands
+//    float moveTo = (2167*x)+1417;         // Original equation
+    float moveTo = (1625*x)+1525;           // Slower movement
+    if (moveTo > max_x) moveTo = max_x;     // Clamp values higher than max
+    else if (moveTo < min_x) moveTo = min_x;    // Clamp values lower than min
     Xbee.print("#0 P");
     Xbee.print(moveTo);
     Xbee.print(" S600 T10");
     Xbee.println("");
 }
 
+// Move Z servo to specified position
 void moveZ(float z){
-    Xbee.println(27, 'i');      // Cancel any previous commands
-//    float moveTo = ((3133*z)-1647);   // Original equation
-    float moveTo = (1880*z)-456;    // Slower movement
-    if (moveTo > max_z) moveTo = max_z;
-    else if (moveTo < min_z) moveTo = min_z;
+    Xbee.println(27, 'i');                  // Cancel any previous commands
+//    float moveTo = ((3133*z)-1647);       // Original equation
+    float moveTo = (1880*z)-456;            // Slower movement
+    if (moveTo > max_z) moveTo = max_z;     // Clamp values higher than max
+    else if (moveTo < min_z) moveTo = min_z;    // Clamp values lower than min
     Xbee.print("#1 P");
     Xbee.print(moveTo);
     Xbee.print(" S600 T10");
     Xbee.println("");
 }
 
+// Connect to Kinect
 void connect(){
     //Read Buffer
     if (Serial.available() == 5) 
@@ -173,11 +196,9 @@ void connect(){
         digitalWrite(ledPin_3, LOW);
         connected = true;
     }
-    /*else{
-        Serial.print("Data received" + inputByte_0);
-    }*/
 }
 
+// Reset arm to initial position
 void reset(){
   Xbee.println("#0 P1890 #1 P1410 #2 P1630 #3 P1300 #4 P1500 S50");
   connected = false;
