760ffc5fb2f255a347c11908046a04cf8f82d750
StormWulf <auronzeroo@aim.com>
2016-03-31 16:02:59 -0400

    Test code for platform control

---
+++ Robot arm/Arduino Code/ArmControl/Platform_control_test/Platform_control_test.ino
@@ -0,0 +1,48 @@
+void setup() {
+  // open the serial port:
+  Serial.begin(9600);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+}
+
+void loop() {
+  // check for incoming serial data:
+  if (Serial.available() > 0) {
+    // read incoming serial data:
+    char inChar = Serial.read();
+    // Type the next ASCII value from what you received:
+    Keyboard.write(inChar + 1);
+	while( inChar == 'W' ){
+	        forward();
+	}
+
+	while( inChar == 'A' ){
+		left();
+	}
+
+	while( inChar == 'S' ){
+		backward();
+	}
+
+	while( inChar == 'D' ){
+		right();
+	}
+  }
+}
+
+void forward(){
+	Serial.println("#31 P1555 #30 P1555 #16 P1555 #17 P1555");
+}
+
+void backward(){
+	Serial.println("#31 P1460 #30 P1460 #16 P1460 #17 P1460");
+}
+
+//left wheels backward, right wheels forward
+void left(){ 
+	Serial.println("#30 P1460 #31 P1460 #16 P1555 #17 P1555");
+}
+//right wheels backward, left wheels forward
+void right(){
+	Serial.println("#16 P1460 #17 P1460 #30 P1555 #31 P1555");
+}
